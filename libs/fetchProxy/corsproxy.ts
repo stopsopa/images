955ced28cbe955a1b0d1c99c0d8de18e8bfc5fd0
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-06-10 15:22:03 +0100

    d

--- libs/fetchProxy/corsproxy.ts
+++ libs/fetchProxy/corsproxy.ts
@@ -1,5 +1,6 @@
 export default async function load(url: string): Promise<Blob> {
-    const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(url)}`;
+    // const proxyUrl = `https://corsproxy.io/?${encodeURIComponent(url)}`;
+    const proxyUrl = `https://corsproxy.io/?url=${encodeURIComponent(url)}`;
     const response = await fetch(proxyUrl);
     if (!response.ok) {
         throw new Error(`Failed to load from corsproxy.io: ${response.statusText}`);
